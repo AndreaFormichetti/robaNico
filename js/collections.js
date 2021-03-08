66192ce8dec2dc7cdb8e6fb0e378bac248223eee
Andrea Formichetti <andrea.formichetti@student.univaq.it>
2021-03-08 19:32:26 +0100

    aggiunte cose array

--- js/collections.js
+++ js/collections.js
@@ -1,17 +1,17 @@
-let arrLiteral = ["zero", "uno", "due", "tre", "quattro"];
+// let arrLiteral = ["zero", "uno", "due", "tre", "quattro"];
 
-console.log("lunghezza array", arrLiteral.length);
+// console.log("lunghezza array", arrLiteral.length);
 
 //scorrimento array tramite for loop
-for (let i = 0; i < arrLiteral.length; i++)
-{
-    console.log("indice:", i);
-    console.log("valore:", arr[i]);
-}
+// for (let i = 0; i < arrLiteral.length; i++)
+// {
+//     console.log("indice:", i);
+//     console.log("valore:", arrLiteral[i]);
+// }
 
 //scorrimento array tramite foreach
 // arrLiteral.forEach(function(e, index) {
-//     console.log("indice:", i);
+//     console.log("indice:", index);
 //     console.log("valore:", e);
 // })
 
@@ -19,5 +19,26 @@ for (let i = 0; i < arrLiteral.length; i++)
 
 // for (let i = 0; i < 10; i++)
 // {
-//     arr.push(Math.random() * (100 - 0));
+//     arr.push((Math.random() * (100 - 1) + 1).toFixed(0));
 // }
+
+// console.log(arr);
+
+// let variabile;
+
+// if (variabile == null) console.log("ciao");
+
+// console.log(variabile);
+
+let tabella = [
+    [1, 2, 3, 4],
+    ["ciao", "salve", "eccetera", "ancora"]
+];
+
+for (let i = 0; i < tabella.length; i++)
+{
+    for (let j = 0; j < tabella[i].length; j++)
+    {
+        console.log(tabella[i][j]);
+    }
+}
